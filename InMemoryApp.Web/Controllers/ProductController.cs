368f795ef7135bd02c22e1491c42571f5bb5e80d
Bayram <byrm.telli.contact@gmail.com>
2024-07-11 10:53:41 +0300

    Add project files.

---
+++ InMemoryApp.Web/Controllers/ProductController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace InMemoryApp.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IMemoryCache _memoryCache;
+
+        public ProductController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+        public IActionResult Index()
+        {
+            //Getting cached data from memory with null check.
+
+            //first method
+            if (String.IsNullOrEmpty(_memoryCache.Get<string>("zaman")))
+            {
+                _memoryCache.Set<string>("zaman", DateTime.Now.ToString());
+            }
+
+            //second method
+            if (!_memoryCache.TryGetValue("zaman",out string zamancache)) 
+            {
+                _memoryCache.Set<string>("zaman", DateTime.Now.ToString());
+            }
+            return View();
+        }
+
+
+        public IActionResult Show()
+        {
+            //third method
+            _memoryCache.GetOrCreate<string>("zaman", entry =>
+            {
+                return DateTime.Now.ToString();
+            });
+
+            ViewBag.zaman = _memoryCache.Get<string>("zaman");
+            return View();
+        }
+    }
+}
